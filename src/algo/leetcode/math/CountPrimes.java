1eabd93b122d29162f82756544a52af0e06486e6
Parkash Arjan <Arjan.Parkash.Git@gmail.com>
2020-07-05 22:21:44 -0400

    revised logic

--- src/algo/leetcode/math/CountPrimes.java
+++ src/algo/leetcode/math/CountPrimes.java
@@ -1,19 +1,19 @@
 package algo.leetcode.math;
 
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class CountPrimes {
 
 	public static void main(String[] args) {
 		CountPrimes countPrimes = new CountPrimes();
-		int count = countPrimes.countPrimes(100);
-		System.out.println(count);
+		Set<Integer> primes = countPrimes.countPrimes(10);
+		System.out.println(primes);
+		System.out.println(primes.size());
 	}
 
-	public int countPrimes(int n) {
+	public Set<Integer> countPrimes(int n) {
 
 		Map<Integer, Boolean> map = new ConcurrentHashMap<>(n);
 
@@ -23,9 +23,7 @@ public class CountPrimes {
 			}
 		}
 
-		for (Map.Entry<Integer, Boolean> entry : map.entrySet()) {
-
-			int key = entry.getKey();
+		for (Integer key : map.keySet()) {
 
 			if (Math.pow(key, 2.0) >= n) {
 				break;
@@ -39,7 +37,35 @@ public class CountPrimes {
 
 		}
 
-		System.out.println(map.keySet());
-		return map.size();
+		return map.keySet();
 	}
+//	public int countPrimes(int n) {
+//
+//		Map<Integer, Boolean> map = new ConcurrentHashMap<>(n);
+//
+//		for (int i = 2; i < n; i++) {
+//			if (i % 2 != 0 || i == 2) {
+//				map.put(i, true);
+//			}
+//		}
+//
+//		for (Map.Entry<Integer, Boolean> entry : map.entrySet()) {
+//
+//			int key = entry.getKey();
+//
+//			if (Math.pow(key, 2.0) >= n) {
+//				break;
+//			}
+//
+//			int multiple = 0;
+//			for (int i = key; multiple < n; i++) {
+//				multiple = key * i;
+//				map.remove(multiple);
+//			}
+//
+//		}
+//
+//		System.out.println(map.keySet());
+//		return map.size();
+//	}
 }
