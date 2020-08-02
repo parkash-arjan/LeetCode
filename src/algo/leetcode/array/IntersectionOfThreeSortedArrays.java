84d253d52f373e571855b5eaabdd99bccebc3d13
Parkash Arjan <Arjan.Parkash.Git@gmail.com>
2020-08-02 19:13:12 -0400

    optimized

--- src/algo/leetcode/array/IntersectionOfThreeSortedArrays.java
+++ src/algo/leetcode/array/IntersectionOfThreeSortedArrays.java
@@ -1,8 +1,10 @@
 package algo.leetcode.array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -17,31 +19,75 @@ public class IntersectionOfThreeSortedArrays {
 	}
 
 	public static List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
-
-		Map<Integer, Integer> map = new HashMap<>();
-
-		fillMap(arr1, map);
-		fillMap(arr2, map);
-		fillMap(arr3, map);
-
-		Set<Entry<Integer, Integer>> values = map.entrySet();
+		int[] temp = intersect(arr1, arr2);
+		int[] intersection = intersect(temp, arr3);
 		List<Integer> result = new ArrayList<>();
-		for (Entry<Integer, Integer> entry : values) {
-			if (entry.getValue() >= 3) {
-				result.add(entry.getKey());
 
-			}
+		for (int i = 0; i < intersection.length; i++) {
+			result.add(intersection[i]);
 		}
 
-		Collections.sort(result);
 		return result;
 	}
 
-	public static void fillMap(int[] arr, Map<Integer, Integer> map) {
-		for (int i = 0; i < arr.length; i++) {
-			map.merge(Integer.valueOf(arr[i]), 1, Integer::sum);
+	public static int[] intersect(int[] nums1, int[] nums2) {
+
+		if (nums1.length > nums2.length) {
+			return intersect(nums2, nums1);
 		}
+
+		// nums1 is less than nums2
+
+		int i = 0;
+		int j = 0;
+
+		int idx = -1;
+		while (i < nums1.length && j < nums2.length) {
+
+			if (nums1[i] == nums2[j]) {
+				nums1[++idx] = nums2[j];
+				i++;
+				j++;
+
+			} else if (nums1[i] < nums2[j]) {
+				i++;
+			} else if (nums1[i] > nums2[j]) {
+				j++;
+
+			}
+
+		}
+
+		return idx == -1 ? new int[0] : Arrays.copyOfRange(nums1, 0, idx + 1);
+
 	}
+
+//	public static List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+//
+//		Map<Integer, Integer> map = new LinkedHashMap<>(Math.min(arr1.length, Math.min(arr2.length, arr3.length)));
+//
+//		fillMap(arr1, map);
+//		fillMap(arr2, map);
+//		fillMap(arr3, map);
+//
+//		Set<Entry<Integer, Integer>> values = map.entrySet();
+//		List<Integer> result = new ArrayList<>();
+//		for (Entry<Integer, Integer> entry : values) {
+//			if (entry.getValue() >= 3) {
+//				result.add(entry.getKey());
+//
+//			}
+//		}
+//
+//		// Collections.sort(result);
+//		return result;
+//	}
+//
+//	public static void fillMap(int[] arr, Map<Integer, Integer> map) {
+//		for (int i = 0; i < arr.length; i++) {
+//			map.merge(Integer.valueOf(arr[i]), 1, Integer::sum);
+//		}
+//	}
 
 //	public static List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
 //
