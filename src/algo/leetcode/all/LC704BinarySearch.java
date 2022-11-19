927ef7fcd0710e350dd1f419c77c72f392c2410f
Parkash Arjan <Arjan.Parkash.Git@gmail.com>
2022-11-19 00:20:35 -0500

    solved

--- src/algo/leetcode/all/LC704BinarySearch.java
+++ src/algo/leetcode/all/LC704BinarySearch.java
@@ -6,15 +6,24 @@ public class LC704BinarySearch {
 
     public static void main(String[] args) {
         LC704BinarySearch runner = new LC704BinarySearch();
-        runner.search(new int[]{-1,0,3,5,9,12}, 9);
-
+        // int result = runner.search(new int[]{-1,0,3,5,9,12}, 9);
+        // int result = runner.search(new int[] { -1, 0, 3, 5, 9, 12 }, 2);
+        int result = runner.search(new int[] { -1, 0, 3, 5, 9, 12 }, 13);
+        Util.print(result);
     }
+
     public int search(int[] nums, int target) {
         int start = 0;
-        int end - nums.length;
-        int mid = (start + end)/2;
-        for (int num : nums) {
-            Util.print(num);
+        int end = nums.length - 1;
+        int mid = (start + end) / 2;
+        while (mid >= start && mid <= end) {
+            if (target == nums[mid])
+                return mid;
+            else if (target < nums[mid])
+                end = mid - 1;
+            else
+                start = mid + 1;
+            mid = (start + end) / 2;
         }
         return -1;
     }
